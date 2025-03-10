4704421214ea90370ddee02d4834e5b2c13ca2a9
Balaji <balajich0582gmail.com>
2025-03-10 11:38:57 +0530

    bcb

---
+++ frontend/filesharing/src/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+      <div>
+    
+    <div>Name :-balaji</div>
+    <div>number:8125685107</div>
+    <div>email:-balajich058@gmail.com</div>
+
+    </div>
+)
+}
+
+export default Home
